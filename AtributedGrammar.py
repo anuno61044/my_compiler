a2cf955edcd45dbeb49688ff979adaf3df23d37b
anuno61044 <anuno61044@gmail.com>
2024-06-19 09:39:07 -0400

    refactoring code

--- AtributedGrammar.py
+++ AtributedGrammar.py
@@ -1,21 +1,21 @@
+
 def evaluate_parse(left_parse, tokens):
+    
     if not left_parse or not tokens:
         return
     
     left_parse = iter(left_parse)
     tokens = iter(tokens)
+    next(tokens)
     result = evaluate(next(left_parse), left_parse, tokens)
     
     return result
     
-
-def evaluate(production, left_parse, tokens, inherited_value=None):
-    head, body = production
-    attributes = production.attributes
     
-    # Insert your code here ...
-    # > synteticed = ...
-    # > inherited = ...
+def evaluate(production, left_parse, tokens, inherited_value=None):
+    _, body = production
+    
+    attributes = production.attributes
     synteticed = []
     inherited = []
     for i in range(len(attributes)):
@@ -23,23 +23,15 @@ def evaluate(production, left_parse, tokens, inherited_value=None):
         inherited.append(None)
     
     inherited[0] = inherited_value
-    for i, symbol in enumerate(body, 1):
+    
+    for i, symbol in enumerate(reversed(body),1):
+        index = len(body)-i
         if symbol.IsTerminal:
-            assert inherited[i] is None
-            if(body[i - 1] == num):
-                synteticed[i] = next(tokens).lex
-                inherited_value = attributes[0](inherited,synteticed)
-                return inherited_value
-            else:
-                synteticed[i] = next(tokens).lex
-                # attributes[i](inherited,synteticed)
+            a = next(tokens).lex
+            synteticed[index+1] = a
         else:
             next_production = next(left_parse)
-            assert symbol == next_production.Left
-            if not attributes[i] == None:
-               synteticed[i] = evaluate(next_production, left_parse, tokens, attributes[i](inherited, synteticed))
-            else :
-                synteticed[i] = evaluate(next_production,left_parse,tokens, inherited_value)
-    # Insert your code here ...
+            synteticed[index+1] = evaluate(next_production,left_parse,tokens, inherited_value)
+
     return attributes[0](inherited, synteticed)
     
