6a07044396881c6c50a688a6c5156dc3cb3041bb
anuno61044 <anuno61044@gmail.com>
2024-06-19 10:38:02 -0400

    add method GefGrammar

--- my_parser.py
+++ my_parser.py
@@ -1,46 +1,57 @@
 from cmp.pycompiler import *
 from TreeDef import *
 from parser_tools import *
-from AtributedGrammar import *
+from cmp.ast import get_printer
 
-G = Grammar()
+def DefGrammar():
 
-# No terminales
-E = G.NonTerminal('E', True)
-A,T,F = G.NonTerminals('A T F')
+    G = Grammar()
 
-# Terminales
-equal, plus, times, minus, div, num, lpar, rpar = G.Terminals('= + * - / float ( )')
+    # No terminales
+    E = G.NonTerminal('E', True)
+    A,T,F = G.NonTerminals('A T F')
 
-# Reglas
-E %= A, lambda h,s : s[1]
+    # Terminales
+    equal, plus, times, minus, div, num, lpar, rpar = G.Terminals('= + * - / float ( )')
 
-A %=  A + plus + T, lambda h,s: PlusNode(s[1],s[3])
-A %=  A + minus + T, lambda h,s: MinusNode(s[1],s[3])
-A %= T, lambda h,s : s[1]
+    # Reglas
+    E %= A, lambda h,s : s[1]
 
-T %= T + times + F, lambda h,s: StarNode(s[1],s[3])
-T %= T + div + F, lambda h,s: DivNode(s[1],s[3])
-T %= F, lambda h,s : s[1]
+    A %=  A + plus + T, lambda h,s: PlusNode(s[1],s[3])
+    A %=  A + minus + T, lambda h,s: MinusNode(s[1],s[3])
+    A %= T, lambda h,s : s[1]
 
-F %= num, lambda h,s : ConstantNumberNode(s[1])
-F %= lpar + A + rpar, lambda h,s : s[2]
+    T %= T + times + F, lambda h,s: StarNode(s[1],s[3])
+    T %= T + div + F, lambda h,s: DivNode(s[1],s[3])
+    T %= F, lambda h,s : s[1]
 
+    F %= num, lambda h,s : ConstantNumberNode(s[1])
+    F %= lpar + A + rpar, lambda h,s : s[2]
+    
+    return G
+
+    
+# *******************************************************************
+# ********************** Ejecutar el parser *************************
+# *******************************************************************
+
+G = DefGrammar()
+
+equal, plus, times, minus, div, num, lpar, rpar = G.terminals
 
 parser = LR1Parser(G, verbose=True)
 
-tokens = [Token('5', num), Token('*', times), Token('(', lpar), Token('2', num), Token('+', plus), Token('4', num), Token(')', rpar), Token('/', div), Token('411', num), Token('$', G.EOF) ]
+tokens = [Token('5', num), Token('+', plus), Token('2', num), Token('$', G.EOF) ]
 derivation = parser([tok.token_type for tok in tokens])
+
+# Como el parser el LR(1) al trabajar sobre la forma de atributar
+# la gramatica LL(1) debemos tomar los tokens y las derivaciones
+# en orden inverso
 
 tokens.reverse()
 derivation.reverse()
 
-print('\n\n',derivation)
-print(tokens)
-print('\n')
-
 result = evaluate_parse(derivation, tokens)
-from cmp.ast import get_printer
+
 printer = get_printer(AtomicNode=ConstantNumberNode, BinaryNode=BinaryNode)
 print(printer(result))
-
--- parser_tools.py
+++ parser_tools.py
@@ -53,6 +53,7 @@ def compute_firsts(G):
                     
     return firsts
 
+
 def expand(item: Item, firsts):
     next_symbol = item.NextSymbol
     if next_symbol is None or not next_symbol.IsNonTerminal:
@@ -79,6 +80,7 @@ def compress(items):
         lookaheads.update(item.lookaheads)
     
     return { Item(x.production, x.pos, set(lookahead)) for x, lookahead in centers.items() }
+
 
 def closure_lr1(items, firsts):
     closure = ContainerSet(*items)
@@ -147,6 +149,7 @@ def build_LR1_automaton(G):
                     
     automaton.set_formatter(multiline_formatter)
     return automaton
+
 
 class ShiftReduceParser:
 
@@ -250,6 +253,7 @@ class LR1Parser(ShiftReduceParser):
     def _register(table, key, value):
         assert key not in table or table[key] == value, 'Shift-Reduce or Reduce-Reduce conflict!!!'
         table[key] = value
+     
        
 class Token:
     """
@@ -272,4 +276,44 @@ class Token:
     
     def __repr__(self):
         return str(self)
+    
+# *******************************************
+# ********** Atributar Gramatica ************
+# *******************************************
+
+
+def evaluate_parse(left_parse, tokens):
+    
+    if not left_parse or not tokens:
+        return
+    
+    left_parse = iter(left_parse)
+    tokens = iter(tokens)
+    next(tokens)
+    result = evaluate(next(left_parse), left_parse, tokens)
+    
+    return result
+    
+def evaluate(production, left_parse, tokens, inherited_value=None):
+    _, body = production
+    
+    attributes = production.attributes
+    synteticed = []
+    inherited = []
+    for i in range(len(attributes)):
+        synteticed.append(None)
+        inherited.append(None)
+    
+    inherited[0] = inherited_value
+    
+    for i, symbol in enumerate(reversed(body),1):
+        index = len(body)-i
+        if symbol.IsTerminal:
+            a = next(tokens).lex
+            synteticed[index+1] = a
+        else:
+            next_production = next(left_parse)
+            synteticed[index+1] = evaluate(next_production,left_parse,tokens, inherited_value)
+
+    return attributes[0](inherited, synteticed)
     
